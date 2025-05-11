a401c669aea2dcf9fcab2909e0f81a61327b236d
MustafaGood <mustafa.swedish@gmail.com>
2025-05-11 20:21:59 +0200

    lagt till  produktrepository med CRUD-operationer

--- src/main/java/com/mustafa/warehouseapi1/repository/ProductRepository.java
+++ src/main/java/com/mustafa/warehouseapi1/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.mustafa.warehouseapi1.model.Product;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
 
 @ApplicationScoped
 public class ProductRepository {
@@ -13,6 +14,16 @@ public class ProductRepository {
 
     public Product find(Long id) {
         return entityManager.find(Product.class, id);
+    }
+
+    public List<Product> findAll() {
+        return entityManager.createQuery("SELECT p FROM Product p", Product.class).getResultList();
+    }
+
+    public List<Product> findByCategory(String category) {
+        return entityManager.createQuery("SELECT p FROM Product p WHERE p.category = :category", Product.class)
+                .setParameter("category", category)
+                .getResultList();
     }
 
     public void save(Product product) {
