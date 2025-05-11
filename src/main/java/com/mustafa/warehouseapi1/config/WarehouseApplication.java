78fdf7444e547ef4c96e94ee9431300e477be2a9
MustafaGood <mustafa.swedish@gmail.com>
2025-05-11 20:20:30 +0200

    Lägg till grundläggande REST API-konfiguration

---
+++ src/main/java/com/mustafa/warehouseapi1/config/WarehouseApplication.java
@@ -0,0 +1,8 @@
+package com.mustafa.warehouseapi1.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class WarehouseApplication extends Application {
+} 
